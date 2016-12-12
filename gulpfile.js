91c25ab034ad0f4d7ebb08c5038563f96afe18c1
ollywilco <olly@wilco.com>
2016-12-12 18:26:58 +0000

    added image compression

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var gulp = require('gulp'),
 		uglify = require('gulp-uglify'),
 		minifyHTML = require('gulp-minify-html'),
 		jsonminify = require('gulp-jsonminify'),
+		imagemin = require('gulp-imagemin'),
+		pngcrush = require('imagemin-pngcrush'),
 		concat = require('gulp-concat');
 
 var env,
@@ -76,6 +78,7 @@ gulp.task('watch', function(){
 	gulp.watch('components/sass/*.scss', ['compass']);
 	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch('builds/development/js/*.json', ['json']);
+	gulp.watch('builds/development/images/**/*.*', ['json']);
 });
 
 gulp.task('connect', function(){
@@ -92,6 +95,18 @@ gulp.task('html', function(){
 		.pipe(connect.reload())
 });
 
+gulp.task('images', function(){
+	gulp.src('builds/development/images/**/*.*')
+		.pipe(gulpif(env === 'production', imagemin({
+			progressive: true,
+			svgoPlugins: [{ removeViewBox: false }],
+			use: [pngcrush()]
+		})))
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+		.pipe(connect.reload())
+});
+
+
 gulp.task('json', function(){
 	gulp.src('builds/development/js/*.json')
 		.pipe(gulpif(env === 'production', jsonminify()))
@@ -99,7 +114,7 @@ gulp.task('json', function(){
 		.pipe(connect.reload())
 });
 
-gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'images', 'watch']);
 
 
 
